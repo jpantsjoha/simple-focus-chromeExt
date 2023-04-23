b0309572f2780ad001cd59726a7686367ef0234f
Jaroslav <Jaroslav@contino.io>
2023-04-23 16:24:00 +0100

    reworking to simplify

--- js/background.js
+++ js/background.js
@@ -1,53 +1,83 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.command === "startTimer") {
-    startTimer(request.duration);
-  } else if (request.command === "togglePauseContinue") {
-    togglePauseContinue();
-  } else if (request.command === "resetTimer") {
-    resetTimer();
+const startButton = document.getElementById("start-timer");
+const stopButton = document.getElementById("stop-timer");
+const timerDisplay = document.getElementById("timer-display");
+const timerSlider = document.getElementById("timer-slider");
+
+let timerId = null;
+
+startButton.addEventListener("click", () => {
+  if (timerId) {
+    clearInterval(timerId);
+    timerId = null;
+  }
+
+  const duration = parseInt(timerSlider.value);
+  let timeRemaining = duration * 60;
+  timerId = setInterval(() => {
+    timeRemaining--;
+
+    if (timeRemaining <= 0) {
+      clearInterval(timerId);
+      timerId = null;
+      alert("Timer Ended");
+    }
+
+    const minutes = Math.floor(timeRemaining / 60);
+    const seconds = timeRemaining % 60;
+    timerDisplay.textContent = `${minutes}:${seconds.toString().padStart(2, "0")}`;
+  }, 1000);
+});
+
+stopButton.addEventListener("click", () => {
+  if (timerId) {
+    clearInterval(timerId);
+    timerId = null;
   }
 });
 
-let timerId = null;
-let timeRemaining = null;
+timerSlider.addEventListener("input", () => {
+  timerDisplay.textContent = `${timerSlider.value}:00`;
+});
 
-function startTimer(duration) {
-  if (timerId) {
-    clearInterval(timerId);
-    timerId = null;
-  }
+// ----- Merging both
 
-  timeRemaining = duration * 60;
-  timerId = setInterval(updateTimer, 1000);
-}
+let startTime;
+let endTime;
+let countdownInterval = null;
+let countdownState = {
+  remainingSeconds: null,
+};
 
-function updateTimer() {
-  timeRemaining--;
-
-  if (timeRemaining <= 0) {
-    clearInterval(timerId);
-    timerId = null;
-    // You can replace this alert with a notification or other user prompt
-    alert("Timer Ended");
-  }
-
-  chrome.runtime.sendMessage({ command: "updateTimerDisplay", timeRemaining: timeRemaining });
-}
-
-function togglePauseContinue() {
-  if (timerId) {
-    clearInterval(timerId);
-    timerId = null;
-  } else {
-    timerId = setInterval(updateTimer, 1000);
-  }
-}
-
-function resetTimer() {
-  if (timerId) {
-    clearInterval(timerId);
-    timerId = null;
-  }
-
-  timeRemaining = null;
-}
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.command === "startTimer") {
+        const duration = message.duration * 60; // Convert minutes to seconds
+        startTime = new Date().getTime();
+        endTime = startTime + duration * 1000;
+        clearInterval(countdownInterval);
+    
+        countdownInterval = setInterval(() => {
+          const currentTime = new Date().getTime();
+          const remainingSeconds = Math.round((endTime - currentTime) / 1000);
+    
+          if (remainingSeconds <= 0) {
+            clearInterval(countdownInterval);
+            countdownState.remainingSeconds = null;
+            // Play the soft ping sound
+            new Audio('clock_alarm.mp3').play();
+          } else {
+            countdownState.remainingSeconds = remainingSeconds;
+            chrome.runtime.sendMessage({
+              command: "updateCountdown",
+              remainingSeconds: remainingSeconds,
+            });
+          }
+        }, 1000);
+      } else if (message.command === "stopTimer") {
+        clearInterval(countdownInterval);
+        countdownState.remainingSeconds = null;
+      } else if (message.command === "requestCountdownState") {
+        sendResponse(countdownState);
+      }
+    });
+    
+    
